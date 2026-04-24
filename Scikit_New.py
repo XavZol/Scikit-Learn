0e164fd0c9513d2ebe4ba17be6b32eaea72cbcd3
XavZol <alansolis707@gmail.com>
2026-04-23 19:52:22 -0600

    Seleccion y Division de Datos

--- Scikit_New.py
+++ Scikit_New.py
@@ -1,32 +1,25 @@
-import numpy as np
 from sklearn.datasets import load_iris
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, SelectFromModel, chi2
+from sklearn.ensemble import RandomForestClassifier
 
-data = np.array([[1 , -1, 2], 
-                [2, 0, 0], 
-                [0, 1, -1]])
-print(data)
+data = load_iris()
+X = data.data
+y = data.target
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-data_escalada = scaler.fit_transform(data)
-print(data_escalada)
+X_entrena, X_prueba, y_entrena, y_prueba = train_test_split(X, y, test_size=0.2, random_state=42)
 
+print("Tamaño del conjunto total:", len(X))
+print("Tamaño del conjunto entrenamiento: ", len(X_entrena))
+print("Tamaño del conjunto pruebas: ", len(X_prueba))
 
-iris = load_iris()
-X = iris.data
-X_escalado = scaler.fit_transform(X)
+selector = SelectKBest(chi2, k=2)
+X_nuevo = selector.fit_transform(X_entrena, y_entrena)
+print(X_entrena[:5])
+print(X_nuevo[:5])
 
-print(X[:5])
-print(X_escalado[:5])
-
-scaler2 = StandardScaler()
-data_escalada2 = scaler2.fit_transform(data)
-print(data_escalada2)
-
-print(np.std(data_escalada2))
-
-categorias = np.array([["rojo"], ["verde"], ["azul"], ["verde"], ["verde"], ["azul"]])
-encoder = OneHotEncoder(sparse_output=False)
-data_codificada = encoder.fit_transform(categorias)
-print(data_codificada)
-
+modelo = RandomForestClassifier(n_estimators=100, random_state=42)
+selector2 = SelectFromModel(modelo)
+X_importante = selector2.fit_transform(X_entrena, y_entrena)
+print(X_entrena[:5])
+print(X_importante[:5])
