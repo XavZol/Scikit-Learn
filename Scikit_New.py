b7c19f81fde7f5088238cd070782c6b9c029936e
XavZol <alansolis707@gmail.com>
2026-04-23 20:15:44 -0600

    Pipelines

--- Scikit_New.py
+++ Scikit_New.py
@@ -2,24 +2,25 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, SelectFromModel, chi2
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline, make_pipeline
 
 data = load_iris()
 X = data.data
 y = data.target
 
-X_entrena, X_prueba, y_entrena, y_prueba = train_test_split(X, y, test_size=0.2, random_state=42)
+X_entrena, X_prueba, y_entrena, y_prueba = train_test_split(X, y, test_size=0.25, random_state=0)
 
-print("Tamaño del conjunto total:", len(X))
-print("Tamaño del conjunto entrenamiento: ", len(X_entrena))
-print("Tamaño del conjunto pruebas: ", len(X_prueba))
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('modelo', LogisticRegression())
+])
 
-selector = SelectKBest(chi2, k=2)
-X_nuevo = selector.fit_transform(X_entrena, y_entrena)
-print(X_entrena[:5])
-print(X_nuevo[:5])
+pipeline.fit(X_entrena, y_entrena)
 
-modelo = RandomForestClassifier(n_estimators=100, random_state=42)
-selector2 = SelectFromModel(modelo)
-X_importante = selector2.fit_transform(X_entrena, y_entrena)
-print(X_entrena[:5])
-print(X_importante[:5])
+y_pred = pipeline.predict(X_prueba)
+puntaje = pipeline.score(X_prueba, y_prueba)
+print(f"Las predicciones son: {y_pred}")
+print(f"La precisión del modelo es: {puntaje: .2f}")
+
